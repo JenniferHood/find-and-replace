f816a78c5f2dc78d064286659fe4fca2e3327082
Ben Cornelis & Jennifer Minetree <student@epicodus.com>
2015-04-22 16:10:29 -0700

    Now find_and_replace will work for one word strings.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,5 +1,9 @@
 class String
-  define_method(:find_and_replace) do |find, replace|
-    'universe'
+  define_method(:find_and_replace) do |search_word, replacement|
+    if self.eql?(search_word)
+      replacement
+    else
+      self
+    end
   end
 end
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -5,4 +5,8 @@ describe("String#find_and_replace") do
   it('replaces a one word string with the second argument if the word equals the first argument') do
     expect('world'.find_and_replace('world', 'universe')).to(eq('universe'))
   end
+
+  it('does not replace the first word if it does not match the first argument') do
+    expect('coffee'.find_and_replace('tea', 'milk')).to(eq('coffee'))
+  end
 end
